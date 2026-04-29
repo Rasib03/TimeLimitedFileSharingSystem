91551049b3eb855520fd854cc38f082e5bfac02b
Rasib <rasib.dev03@gmail.com>
2026-04-29 19:04:26 +0500

    Add requirements.txt and production settings for PythonAnywhere

--- core/settings.py
+++ core/settings.py
@@ -3,9 +3,15 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-n(1s6+hc)r@487z=65rnx$a27y1(qpiv3vg5!aay_u*j&09&hh'
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-n(1s6+hc)r@487z=65rnx$a27y1(qpiv3vg5!aay_u*j&09&hh')
+
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.pythonanywhere.com',   # covers yourusername.pythonanywhere.com
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -72,6 +78,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
